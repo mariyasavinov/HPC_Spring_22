880cda105f89e2365f3bb2236f385a0515b7c26b
Mariya Savinov <mas10009@nyu.edu>
2022-03-07 12:24:25 -0500

    Solved issues related to val_test01

--- hw2/val_test01_solved.cpp
+++ hw2/val_test01_solved.cpp
@@ -77,13 +77,26 @@ void f ( int n )
   x[1] = 1;
   cout << "  " << 1 << "  " << x[1] << "\n";
 
-  for ( i = 2; i <= n; i++ )
+  /*
+  for ( i = 2; i <= n; i++ )  --> this results in "Invalid write of size 4"
+	because x is an n-size array with entries x[0], x[1], ..., x[n-1]
+	i.e. there is no entry x[n]
+  Instead, let i=2 to i=n-1:
+  */
+  for ( i = 2; i < n; i++ ) 
   {
     x[i] = x[i-1] + x[i-2];
     cout << "  " << i << "  " << x[i] << "\n";
   }
 
-  delete [] x;
+  /*
+  delete [] x;   --> the delete[] operator is used to destroy objects
+	created with new[]  (as well as free the associated allocated memory
+
+  Since x was NOT created with new[], we just need to free the allocated memory,
+	which can be done with:
+  */
+  free(x);
 
   return;
 }
